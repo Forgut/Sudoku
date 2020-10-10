fc793e67d3c7a060f79d26712c16446753db2625
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-10-10 14:30:39 +0200

    Add wpf initialized project with menu

---
+++ Sudoku.Frontend/Views/Menu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sudoku.Frontend.Views
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void SolveSudokuButtonClick(object sender, RoutedEventArgs e)
+        {
+            ShowNotImplementedInfoBox();
+        }
+
+        private void SolveByAIClick(object sender, RoutedEventArgs e)
+        {
+            ShowNotImplementedInfoBox();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ShowNotImplementedInfoBox()
+        {
+            MessageBox.Show($"This function is not implemented yet. Stay tuned.");
+        }
+    }
+}
